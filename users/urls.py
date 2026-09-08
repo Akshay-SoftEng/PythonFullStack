94a1940e32da4ddf4064cd3dc4ca565614d8d9a8
Akshay-learn <akshaypotta123@gmail.com>
2026-09-08 13:18:10 -0400

    All files added

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include,path
-from .views import home, about,hello,welcome,welcome_home, profile
+from .views import home, about,hello,welcome,welcome_home, profile, users,register, session_exit, get_user_data, get_users_data
 urlpatterns = [
-  path("home/", home),
+  path("home/", home, name = "home"),
   path("about/", about),
   path("hello/<str:name>/",hello),
   path("welcome/<str:name>/",welcome),
   path("welcome/",welcome_home),
-  path("profile/<str:name>/", profile)
-]
+  path("profile/<str:name>/", profile),
+  path("users/",users),
+  path("register/",register, name="register"),
+  path("exit/",session_exit, name="exit"),
+  path("get_users/",get_users_data),
+  path("get_user/<int:id>",get_user_data)
+  ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
+from .models import UserProfile
 
 # Create your views here.
 def  home(request):
-  return render(request,"home.html")
+   if request.session.get("is_registered"):
+    return render(request,"home.html")
+   return redirect(register)
 
 def about(request):
   return JsonResponse({"message":"Welcome to about section"})
@@ -17,10 +20,56 @@ def welcome(request,name):
                           <p> This is welcome page</p1>
                        """)
 def welcome_home(request):
-  return render(request,"home.html")
+ 
+    return render(request,"home.html")
 
 def profile(request,name):
   is_admin =True
   email = f"{name}@exmaple.com"
   return render(request, "profile.html",
                 {"name": name , "email": email, "city": "Newyork", "is_admin": is_admin})
+
+def users(request):
+    users = [
+      "Akshay",
+      "Potta",
+      "Sheero",
+      "hima"
+    ]
+    return render(request, "users.html", {"users":users})
+
+def register(request):
+  if request.method == "POST":
+    name = request.POST["name"]
+    email = request.POST["email"]
+
+    request.session["is_registered"]=True
+    request.session["name"]=name
+    request.session["email"]=email
+
+    print("Name:", name)
+    print("email:", email)
+
+    return render(request, 'home.html',{"name":name, "email":email})
+  return render(request, 'register.html')
+
+
+def session_exit(request):
+  # request.session["is_registered"]=False
+   request.session.flush()
+   return redirect(register)
+
+def get_users_data(request):
+  data = UserProfile.objects.all()
+  return JsonResponse({
+          "Users":list(data.values())
+    })
+
+def get_user_data(request, id):
+  data = UserProfile.objects.get(id=id)
+ # return JsonResponse({
+  #        "id":data.id,
+    #      "Name":data.name,
+     #     "email":data.email
+ #   })
+  return JsonResponse(data)
